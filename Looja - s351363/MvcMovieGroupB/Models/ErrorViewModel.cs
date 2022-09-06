ece7169431b3f8f1a227aa62762944c05a4ab54d
Looja Tuladhar <looja1999@gmail.com>
2022-09-06 11:48:08 +0930

    MVC Application

---
+++ Looja - s351363/MvcMovieGroupB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcMovieGroupB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
