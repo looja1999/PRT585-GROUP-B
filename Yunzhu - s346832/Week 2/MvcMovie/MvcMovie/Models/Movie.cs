ce91a0eec83d3d51d057fb11d7c47511787b68d8
Yunzhu Wang <80518131+s346832@users.noreply.github.com>
2022-09-07 15:09:37 +0930

    Practice on task A

---
+++ Yunzhu - s346832/Week 2/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string? Rating { get; set; }
+    }
+    public class MovieGenreViewModel
+    {
+        public List<Movie>? Movies { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? MovieGenre { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
