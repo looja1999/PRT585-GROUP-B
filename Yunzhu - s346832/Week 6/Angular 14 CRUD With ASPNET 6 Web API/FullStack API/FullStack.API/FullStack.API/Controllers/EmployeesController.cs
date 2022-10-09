41d6da57fab3dcf80d90583052a31f82924ed87d
Yunzhu Wang <80518131+s346832@users.noreply.github.com>
2022-10-09 11:19:01 +0930

    Angular 14 CRUD with .NET 6 Web API using Entity Framework Core

--- Yunzhu - s346832/Week 6/Angular 14 CRUD With ASPNET 6 Web API/FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
+++ Yunzhu - s346832/Week 6/Angular 14 CRUD With ASPNET 6 Web API/FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -32,5 +32,52 @@ namespace FullStack.API.Controllers
 
             return Ok(employeeRequest);
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _fullStackDbContext.Employees.Remove(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
     }
 }
--- Yunzhu - s346832/Week 6/Angular 14 CRUD With ASPNET 6 Web API/FullStack UI/FullStack.UI/src/app/services/employees.service.ts
+++ Yunzhu - s346832/Week 6/Angular 14 CRUD With ASPNET 6 Web API/FullStack UI/FullStack.UI/src/app/services/employees.service.ts
@@ -20,4 +20,16 @@ export class EmployeesService {
     addEmployeeRequest.id = '00000000-0000-0000-0000-000000000000'
     return this.http.post<Employee[]>(this.baseApirUrl + 'api/employees', addEmployeeRequest);
   }
+
+  getEmployee(id: string): Observable<Employee> {
+    return this.http.get<Employee>(this.baseApirUrl + 'api/employees/' +  id)
+  }
+
+  updateEmployee(id: string, updateEmployeeRequest: Employee): Observable<Employee>{
+    return this.http.put<Employee>(this.baseApirUrl + 'api/employees/' +  id, updateEmployeeRequest);
+  }
+
+  deleteEmployee(id: string): Observable<Employee>{
+    return this.http.delete<Employee>(this.baseApirUrl + 'api/employees/' +  id);
+  }
 }
