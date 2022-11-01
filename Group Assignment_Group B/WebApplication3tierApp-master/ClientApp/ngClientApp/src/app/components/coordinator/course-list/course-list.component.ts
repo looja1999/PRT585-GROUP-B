214f0f7070cae9141fef768301ba283c364723fb
Yunzhu Wang <80518131+s346832@users.noreply.github.com>
2022-11-01 21:39:42 +0930

    Add Kendo table

--- Group Assignment_Group B/WebApplication3tierApp-master/ClientApp/ngClientApp/src/app/components/coordinator/course-list/course-list.component.ts
+++ Group Assignment_Group B/WebApplication3tierApp-master/ClientApp/ngClientApp/src/app/components/coordinator/course-list/course-list.component.ts
@@ -5,19 +5,29 @@ import { CourseListService } from 'src/app/services/course-list.service';
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
-  styleUrls: ['./course-list.component.less']
+  styleUrls: ['./course-list.component.less'],
+  template: `
+    <kendo-grid [data]="gridData">
+      <kendo-grid-column field="courseId" title="Course ID"> </kendo-grid-column>
+      <kendo-grid-column field="courseCode" title="Course Code"> </kendo-grid-column>
+      <kendo-grid-column field="courseName" title="Course Name">
+      </kendo-grid-column>
+    </kendo-grid>
+  `,
 })
 export class CourseListComponent implements OnInit {
 
   constructor(private courselistService: CourseListService) {} 
   
   courselist:CourseList[] = []
+  public gridData = [];
   
   ngOnInit(): void {
     this.courselistService.getAllCourses().subscribe({
       next: (courselist) => {
         this.courselist = courselist;
         console.log(courselist);
+        this.gridData = courselist;
       },
     });
   }
