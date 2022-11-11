722de3a7bf2a74c6b0532bcbf37c7f7656fea4ba
Yunzhu Wang <80518131+s346832@users.noreply.github.com>
2022-11-11 15:31:03 +0930

    omport ionic pwa element to index.tsx

--- Yunzhu - s346832/Week 11/Ionic with React/photo-gallery/src/index.tsx
+++ Yunzhu - s346832/Week 11/Ionic with React/photo-gallery/src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -21,3 +23,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
